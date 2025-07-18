4c81629b1d85bd049614219bfbb6a910f642512a
Janio Florencio dos Santos <info.janio@gmail.com>
2025-07-18 13:10:25 -0300

    url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -5,7 +5,6 @@ import { env } from "@/env";
 
 export const api = axios.create({
   baseURL: env.VITE_API_URL,
-  withCredentials: true,
 });
 
 // Interceptor para incluir token sempre antes da requisição
